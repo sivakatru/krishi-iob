a53e2fdb929cd0d1e90cd734d4f31961de71828d
sivakatru <sivakatru@gmail.com>
2021-05-31 09:58:40 +0530

    Update firebaseConfig.tsx

--- src/firebaseConfig.tsx
+++ src/firebaseConfig.tsx
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA9f5h1idIIDlo2O1JO9slGuEDBb39QAfk",
-    authDomain: "coronakiller-415dc.firebaseapp.com",
-    databaseURL: "https://coronakiller-415dc.firebaseio.com",
-    projectId: "coronakiller-415dc",
-    storageBucket: "coronakiller-415dc.appspot.com",
-    messagingSenderId: "97044433060",
-    appId: "1:97044433060:web:bd31f8053d0ba3a8c8be31",
-    measurementId: "G-CMM9CS9L26"
+    apiKey: "*",
+    authDomain: "*",
+    databaseURL: "*",
+    projectId: "*",
+    storageBucket: "*",
+    messagingSenderId: "*",
+    appId: "*",
+    measurementId: "*"
   };
 
 firebase.initializeApp(firebaseConfig);
